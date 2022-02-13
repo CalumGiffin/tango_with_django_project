926f4e669a7f00f9044d77f0fb15a6c220ed0e3f
Calum Giffin <2407426g@student.gla.ac.uk>
2022-02-13 11:55:28 +0000

    chapter6finalcommit

--- rango/views.py
+++ rango/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rango.models import Category, Page
-
+from rango.models import Category
+from rango.models import Page
 def index(request):
     
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
+    
     return render(request, 'rango/index.html', context=context_dict)
     
 def about(request):
@@ -17,7 +20,7 @@ def about(request):
 
 def show_category(request, category_name_slug):
 
-    
+    context_dict = {}
     try:
 
         category = Category.objects.get(slug=category_name_slug)
